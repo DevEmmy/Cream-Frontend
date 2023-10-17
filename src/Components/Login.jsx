37a779bb2c2d2a1cbac04a2a9af583f41527b2c0
Folayimi <ridwanfolayimi@gmail.com>
2023-10-17 18:18:39 +0100

    made some changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,48 +1,76 @@
-import { login } from '@/services/request'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import { login } from "@/services/request";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const Login = () => {
-
   const [details, setDetails] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const handleChange = (e)=>{
-      setDetails({...details, [e.target.name]: e.target.value}) 
-  }
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
 
-  const submitForm = async (e)=>{
-    e.preventDefault()
+  const submitForm = async (e) => {
+    e.preventDefault();
     await login(details.email, details.password, router);
-  }
+  };
 
   return (
-    <div className='bg-slate-700 w-full h-[100vh] flex flex-col gap-3 items-center justify-center'>
-        <div className="bg-white cflexsm sm:gap-5 w-[40%] sm:w-[90%] h-[70vh] sm:rounded-xl sm:h-[80vh] p-10 sm:p-5 text-center">
-            <h2 className='text-[2em] font-[700]'>CREAM</h2>
-            <p>Welcome Back, We missed you!</p>
+    <div className="bg-slate-700 w-full h-[100vh] flex flex-col gap-3 items-center justify-center">
+      <div className="bg-white cflexsm sm:gap-5 w-[40%] sm:w-[90%] h-[70vh] sm:rounded-xl sm:h-[80vh] p-10 sm:p-5 text-center">
+        <h2 className="text-[2em] font-[700]">CREAM</h2>
+        <p>Welcome Back, We missed you!</p>
 
-            <form action="" className='flex w-full gap-5 flex-col'>
-                <input type="text" placeholder='johndoe@gmail.com' className='w-full text-[0.8em] p-3' name="email" value={details["email"]} onChange={handleChange}/>
-                <input type="password" placeholder='**** ' className='w-full text-[0.8em] p-3' name="password" value={details["password"]} onChange={handleChange}/>
-                
-                <p className='text-start'>Forgotten Password? <Link href={"/forgot-password"} className='text-primary1 font-[500]'>Click Here</Link> </p>
-            </form>
+        <form action="" className="flex w-full gap-5 flex-col">
+          <input
+            type="text"
+            placeholder="johndoe@gmail.com"
+            className="w-full text-[0.8em] p-3"
+            name="email"
+            value={details["email"]}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="**** "
+            className="w-full text-[0.8em] p-3"
+            name="password"
+            value={details["password"]}
+            onChange={handleChange}
+          />
 
-            <button className='bg-primary1 w-full py-3 text-black rounded-md my-3' onClick={submitForm}>
-                Login
-            </button>
+          <p className="text-start">
+            Forgotten Password?{" "}
+            <Link
+              href={"/forgot-password"}
+              className="text-primary1 font-[500]"
+            >
+              Click Here
+            </Link>{" "}
+          </p>
+        </form>
 
-            <p>Don't have an account? <Link href={"/register"} className='text-primary1 font-[500]'>Register</Link> </p>
-        </div>
+        <button
+          className="bg-primary1 w-full py-3 text-black rounded-md my-3"
+          onClick={submitForm}
+        >
+          Login
+        </button>
 
+        <p>
+          Don't have an account?{" "}
+          <Link href={"/register"} className="text-primary1 font-[500]">
+            Register
+          </Link>{" "}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,51 +1,92 @@
-import { register } from '@/services/request'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import { register } from "@/services/request";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const Register = () => {
+  const router = useRouter();
 
-  const router = useRouter()
-  
   const [details, setDetails] = useState({
     firstName: "",
-    lastName:"",
+    lastName: "",
     email: "",
     phoneNumber: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  const handleChange = (e)=>{
-    setDetails({...details, [e.target.name]: e.target.value}) 
-}
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
 
-const submitForm = async (e)=>{
-  e.preventDefault()
-  await register(details, router);
-}
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await register(details, router);
+  };
   return (
-    <div className='bg-slate-700 w-full h-[100vh] flex flex-col items-center justify-center'>
-        <div className=" bg-white w-[40%] h-[auto] p-10 text-center">
-            <h2 className='text-[2em] font-[700]'>CREAM</h2>
-            <p>Welcome to CREAM!</p>
+    <div className="bg-slate-700 w-full h-[100vh] flex flex-col items-center justify-center">
+      <div className=" bg-white w-[40%] h-[auto] sm:w-[90%] p-10 sm:p-5 text-center">
+        <h2 className="text-[2em] font-[700]">CREAM</h2>
+        <p>Welcome to CREAM!</p>
 
-            <form action="" className='flex gap-5 flex-col my-3'>
-                <input type="text" placeholder='Your first name e.g John' className='w-full text-[0.8em] p-3' name='firstName' onChange={handleChange} value={details["firstName"]}/>
-                <input type="text" placeholder='Your last name e.g Doe' className='w-full text-[0.8em] p-3' name='lastName' onChange={handleChange} value={details.lastName}/>
-                <input type="email" placeholder='Your email e.g johndoe@gmail.com' className='w-full text-[0.8em] p-3' name='email' onChange={handleChange} value={details.email}/>
-                <input type="tel" placeholder='090xxx' className='w-full text-[0.8em] p-3' name='phoneNumber' onChange={handleChange} value={details.phoneNumber}/>
-                <input type="text" placeholder='**** ' className='w-full text-[0.8em] p-3' name='password' onChange={handleChange} value={details.password}/>
-            </form>
+        <form action="" className="flex gap-5 flex-col my-3">
+          <input
+            type="text"
+            placeholder="Your first name e.g John"
+            className="w-full text-[0.8em] p-3"
+            name="firstName"
+            onChange={handleChange}
+            value={details["firstName"]}
+          />
+          <input
+            type="text"
+            placeholder="Your last name e.g Doe"
+            className="w-full text-[0.8em] p-3"
+            name="lastName"
+            onChange={handleChange}
+            value={details.lastName}
+          />
+          <input
+            type="email"
+            placeholder="Your email e.g johndoe@gmail.com"
+            className="w-full text-[0.8em] p-3"
+            name="email"
+            onChange={handleChange}
+            value={details.email}
+          />
+          <input
+            type="tel"
+            placeholder="090xxx"
+            className="w-full text-[0.8em] p-3"
+            name="phoneNumber"
+            onChange={handleChange}
+            value={details.phoneNumber}
+          />
+          <input
+            type="text"
+            placeholder="**** "
+            className="w-full text-[0.8em] p-3"
+            name="password"
+            onChange={handleChange}
+            value={details.password}
+          />
+        </form>
 
-            <button className='bg-primary1 w-full py-3 text-black rounded-md my-3' onClick={submitForm}>
-                Register
-            </button>
+        <button
+          className="bg-primary1 w-full py-3 text-black rounded-md my-3"
+          onClick={submitForm}
+        >
+          Register
+        </button>
 
-            <p>Already Have an Account? <Link href={"/login"} className='text-primary1 font-[500]'>Login</Link> </p>
-        </div>
-
+        <p>
+          Already Have an Account?{" "}
+          <Link href={"/login"} className="text-primary1 font-[500]">
+            Login
+          </Link>{" "}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
