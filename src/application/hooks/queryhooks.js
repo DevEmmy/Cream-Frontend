75c4daa4944a1ac1de9b9521f0ddf538adb506af
Folayimi <ridwanfolayimi@gmail.com>
2023-11-08 22:51:28 +0100

    made some changes

--- src/application/hooks/queryhooks.js
+++ src/application/hooks/queryhooks.js
@@ -3,9 +3,6 @@ import { useEffect } from "react";
 import services from "../../ioc/services";
 
 export const useGetUserDetails = () => {
-    let user = ""
-  useEffect(() => {
-     user = JSON.parse(localStorage.getItem("user"));
-  },[]);
+  const user = JSON.parse(localStorage.getItem("user"));
   return user;
 };
